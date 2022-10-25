2fa2c449e560469a8d915e8b90e9af94401091da
yxj0312 <yanxiajun@hotmail.com>
2022-10-25 22:39:33 +0200

    change product output to resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
@@ -22,6 +23,7 @@ class ProductController extends Controller
 
     public function apiIndex()
     {
+        return ProductResource::collection(Product::all());
         return Product::all();
     }
 
