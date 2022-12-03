bd457db5cde6595c0aaf93b5efc2a0eaf1d73a7d
yxj0312 <yanxiajun@hotmail.com>
2022-12-03 22:20:25 +0100

    fix db bugs by orders table

--- database/migrations/2022_11_27_214136_create_orders_table.php
+++ database/migrations/2022_11_27_214136_create_orders_table.php
@@ -15,24 +15,24 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            // $table->integer('user_id')->unsigned()->nullable();
-            // $table->foreign('user_uid')->on('users');
-            // $table->string('billing_email')->nullable();
-            // $table->string('billing_name')->nullable();
-            // $table->string('billing_address')->nullable();
-            // $table->string('billing_city')->nullable();
-            // $table->string('billing_province')->nullable();
-            // $table->string('billing_postalcode')->nullable();
-            // $table->string('billing_phone')->nullable();
-            // $table->string('billing_name_on_card')->nullable();
-            // $table->integer('billing_discount')->default(0);
-            // $table->string('billing_discount_code')->nullable();
-            // $table->integer('billing_subtotal');
-            // $table->integer('billing_tax');
-            // $table->integer('billing_total');
-            // $table->string('payment_gateway')->default('stripe');
-            // $table->boolean('shipped')->default(false);
-            // $table->string('error')->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('billing_email')->nullable();
+            $table->string('billing_name')->nullable();
+            $table->string('billing_address')->nullable();
+            $table->string('billing_city')->nullable();
+            $table->string('billing_province')->nullable();
+            $table->string('billing_postalcode')->nullable();
+            $table->string('billing_phone')->nullable();
+            $table->string('billing_name_on_card')->nullable();
+            $table->integer('billing_discount')->default(0);
+            $table->string('billing_discount_code')->nullable();
+            $table->integer('billing_subtotal');
+            $table->integer('billing_tax');
+            $table->integer('billing_total');
+            $table->string('payment_gateway')->default('stripe');
+            $table->boolean('shipped')->default(false);
+            $table->string('error')->nullable();
             $table->timestamps();
         });
     }
