ec97af865cd92efcac1193153868c2bb4ef0592f
yxj0312 <yanxiajun@hotmail.com>
2023-05-20 22:01:37 +0200

    update product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,15 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'brand_id',
+        'category_id',
+        'description',
+        'price',
+        'quantity',
+        'image',
+        'is_featured',
+        'is_active',
+        'meta_title',
+        'meta_description',
+        'slug',
+        'featured_image',
+    ];
 
-    public function categories()
+    /**
+     * Get the brand associated with the product.
+     */
+    public function brand()
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsTo(Brand::class);
+    }
+
+    /**
+     * Get the category associated with the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
