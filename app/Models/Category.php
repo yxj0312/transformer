1e2eddd23650dcd7cfe2c6a13baf981e652e6e50
yxj0312 <yanxiajun@hotmail.com>
2023-05-22 22:27:37 +0200

    add category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description', 'image', 'is_active', 'meta_title', 'meta_description', 'order', 'parent_id'
+    ];
 
+    /**
+     * Get the parent category of the category.
+     */
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    /**
+     * Get the child categories of the category.
+     */
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    /**
+     * Get the products under the category.
+     */
     public function products()
     {
-        return $this->belongsToMany(Product::class);
+        return $this->hasMany(Product::class);
     }
 }
