802a54865697567163c7f28052670335209e0853
yxj0312 <yanxiajun@hotmail.com>
2023-06-18 22:11:30 +0200

    add address test and factory

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Address extends Model
 {
-    use SoftDeletes;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'user_id',
---
+++ tests/Unit/AddressTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Address;
+use App\Models\Cart;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AddressTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_create_address()
+    {
+        $user = User::factory()->create();
+
+        $addressData = [
+            'user_id' => $user->id,
+            'address' => '123 Main St',
+            'city' => 'New York',
+            'state' => 'NY',
+            'country' => 'USA',
+            'postal_code' => '10001',
+            'phone_number' => '12345678',
+        ];
+
+        $address = Address::create($addressData);
+
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertDatabaseHas('addresses', $addressData);
+    }
+
+    public function test_update_address()
+    {
+        $address = Address::factory()->create();
+
+        $newData = [
+            'address' => '456 Elm St',
+            'city' => 'Los Angeles',
+            'state' => 'CA',
+            'country' => 'USA',
+            'postal_code' => '90001',
+        ];
+
+        $address->update($newData);
+
+        $this->assertDatabaseHas('addresses', $newData);
+    }
+
+    public function test_delete_address()
+    {
+        $address = Address::factory()->create();
+
+        $address->delete();
+
+        $this->assertSoftDeleted('addresses', ['id' => $address->id]);
+    }
+
+    public function test_belongs_to_user()
+    {
+        $user = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
+
+        $this->assertInstanceOf(User::class, $address->user);
+        $this->assertEquals($user->id, $address->user->id);
+    }
+
+    public function test_has_many_carts()
+    {
+        $address = Address::factory()->create();
+        $cart1 = Cart::factory()->create(['address_id' => $address->id]);
+        $cart2 = Cart::factory()->create(['address_id' => $address->id]);
+
+        $this->assertInstanceOf(Cart::class, $address->carts->first());
+        $this->assertCount(2, $address->carts);
+    }
+
+    // Add any other tests here
+}
