6ced6b228b40caefde74f13ca35e8299f68ddea1
yxj0312 <yanxiajun@hotmail.com>
2023-06-27 22:00:02 +0200

    add order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'payment_method_id',
+        'order_status_id',
+        'total_amount',
+        'currency',
+        'payment_status',
+        'shipping_method',
+        'tracking_number',
+        'shipping_address',
+        'billing_address',
+    ];
+
+    /**
+     * Get the user that placed the order.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the payment method of the order.
+     */
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+
+    /**
+     * Get the status of the order.
+     */
+    public function orderStatus()
+    {
+        return $this->belongsTo(OrderStatus::class);
+    }
+
+    /**
+     * Get the products in the order.
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_product')->withPivot('quantity');
+    }
 }
