a48d02d3354e658a8f3ba512d498c0e97104cb95
yxj0312 <yanxiajun@hotmail.com>
2023-07-19 22:07:43 +0200

    remove unused codes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
     }
 
     /**
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,12 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    // protected function setUp(): void
-    // {
-    //     parent::setUp();
-
-    //     // 绑定 UserRepositoryInterface 到 UserRepository
-    //     $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-    // }
 }
