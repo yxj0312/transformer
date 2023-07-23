c83e684f65f15b1787ad576a94e237f672fc065a
yxj0312 <yanxiajun@hotmail.com>
2023-07-23 22:09:01 +0200

    test vite detective

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,22 +14,9 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 text-sm">
+        123
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navbar')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="test-css">123</div>
         </div>
     </body>
 </html>
