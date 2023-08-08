393edfddd36f5730befdfbe0b2a955b68471ac2a
yxj0312 <yanxiajun@hotmail.com>
2023-08-08 22:54:59 +0200

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+const { createVuePlugin } = require('vite-plugin-vue');
+const { createVueJsxPlugin } = require('vite-plugin-vue-jsx');
 
 export default defineConfig({
     plugins: [
+        createVuePlugin(), // Vite Vue 插件
+        createVueJsxPlugin(), // 支持 Vue JSX
         laravel({
             input: [
                 'resources/css/app.css',
