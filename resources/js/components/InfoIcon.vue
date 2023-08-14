eed8c13824cdc8153fd1e09cdd0831499dc84715
yxj0312 <yanxiajun@hotmail.com>
2023-08-14 22:21:29 +0200

    update form

---
+++ resources/js/components/InfoIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="text-gray-500 text-sm cursor-pointer" @mouseover="showInfo">{{ icon }}</span>
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  text: String,
+});
+
+const icon = ref('ℹ️');
+
+const showInfo = () => {
+  console.log(props.text);
+};
+</script>
--- resources/js/components/RegistrationForm.vue
+++ resources/js/components/RegistrationForm.vue
@@ -1,38 +1,73 @@
 <template>
   <form @submit.prevent="registerUser" class="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg">
-    <!-- <h2 class="text-2xl font-semibold mb-4">Register</h2> -->
+     <div class="grid grid-cols-2 gap-4">
+      <div>
+        <label class="font-semibold">Name:</label>
+        <input v-model="formData.name" class="w-full border rounded-md p-2">
+      </div>
+      
+      <div>
+        <label class="font-semibold">Email:</label>
+        <input v-model="formData.email" type="email" class="w-full border rounded-md p-2">
+      </div>
+    </div>
     
-    <label class="block mb-4">
-      Name:
-      <input v-model="formData.name" class="w-full border rounded-md p-2">
-    </label>
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <label class="font-semibold">Password:</label>
+        <input v-model="formData.password" type="password" class="w-full border rounded-md p-2">
+      </div>
+      
+      <div>
+        <label class="font-semibold">Role:</label>
+        <select v-model="formData.role_id" class="w-full border rounded-md p-2">
+          <option value="admin">Admin</option>
+          <option value="customer">Customer</option>
+          <option value="vendor">Vendor</option>
+        </select>
+      </div>
+    </div>
     
-    <label class="block mb-4">
-      Email:
-      <input v-model="formData.email" type="email" class="w-full border rounded-md p-2">
-    </label>
+    <div>
+      <label class="font-semibold">Date of Birth:</label>
+      <div class="relative">
+        <input v-model="formData.date_of_birth" type="date" class="w-full border rounded-md p-2">
+        <info-icon text="Your date of birth" class="absolute top-0 right-0" />
+      </div>
+    </div>
     
-    <label class="block mb-4">
-      Password:
-      <input v-model="formData.password" type="password" class="w-full border rounded-md p-2">
-    </label>
+    <div>
+      <label class="font-semibold">First Name:</label>
+      <div class="relative">
+        <input v-model="formData.first_name" class="w-full border rounded-md p-2">
+        <info-icon text="Your first name" class="absolute top-0 right-0" />
+      </div>
+    </div>
     
-    <label class="block mb-4">
-      Role:
-      <select v-model="formData.role_id" class="w-full border rounded-md p-2">
-        <option value="admin">Admin</option>
-        <option value="customer">Customer</option>
-        <option value="vendor">Vendor</option>
-      </select>
-    </label>
+    <div>
+      <label class="font-semibold">Last Name:</label>
+      <div class="relative">
+        <input v-model="formData.last_name" class="w-full border rounded-md p-2">
+        <info-icon text="Your last name" class="absolute top-0 right-0" />
+      </div>
+    </div>
     
-    <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Register</button>
+    <div>
+      <label class="font-semibold">Phone:</label>
+      <div class="relative">
+        <input v-model="formData.phone" class="w-full border rounded-md p-2">
+        <info-icon text="Your phone number" class="absolute top-0 right-0" />
+      </div>
+    </div>
+    
+    <button type="submit" class="mt-10 w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">Register</button>
   </form>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import InfoIcon from './InfoIcon.vue';
 
 const formData = ref({
   name: '',
