a2437c4308d19b69bcec949619257047d3022477
yxj0312 <yanxiajun@hotmail.com>
2023-09-07 22:17:30 +0200

    install vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,13 @@
 import './bootstrap';
 import '../css/app.css'; 
 
-// import Alpine from 'alpinejs';
-
-// window.Alpine = Alpine;
-
-// Alpine.start();
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
-import App from './views/App.vue'; // 这里假设你的根组件为 App.vue
+import App from './views/App.vue'; //
+import router from "./router";  这里假设你的根组件为 App.vue
+
 
 const pinia = createPinia()
-createApp(App).use(pinia).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
 
